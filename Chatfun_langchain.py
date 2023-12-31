a907de13b7eb6cd9102a9a9d2e00c6265e91f8bb
Madhu Ram <madhu.ram@intel.com>
2023-12-31 11:13:13 -0800

    ch

--- Chatfun_langchain.py
+++ Chatfun_langchain.py
@@ -13,13 +13,13 @@ open_api_key = st.secrets.get("OPEN_API_KEY")  # st.sidebar.text_input('OpenAI A
 def generate_response(input_text):
    llm = OpenAI(temperature=0.7, openai_api_key=open_api_key)
    output = llm(input_text)
-   st.info(output.replace(r"\n", "  "))
+   st.write(output.replace(r"\n", r"  \n"))
    # with st.chat_message("assistant"):
    #    st.write(output)
    print(output)
 
 with st.form('my_form'):
-   text = st.text_area('Enter text:', 'Write a 1 stanza poem about the insanity of ')
+   text = st.text_area('Enter text:', 'Write a 1 stanza poem about the beauty of pain')
    if submitted := st.form_submit_button('Submit'):
    # if not open_api_key.startswith('sk-'):
    #    st.warning('Please enter your OpenAI API Key!')
